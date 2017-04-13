bd0b0ba8cd14b7fa087a649c2a40ead66f51fc06
Alessandro Resta <alessandro.resta@gmail.com>
2017-04-13 18:57:42 +0100

    Remove neo4j spin up scripts. Now on storage repo

--- resources/scripts/rm-container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-set -u
-
-CONTAINER=$1
-
-docker stop ${CONTAINER} 2> /dev/null || true && \
-  docker rm ${CONTAINER} 2> /dev/null || true
--- resources/scripts/rm-image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -u
-
-IMAGE=$1
-
-docker rmi -f ${IMAGE} 2> /dev/null
-
-exit 0
