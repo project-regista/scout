049f97f414ca7714633676efe0f41a56ee14d194
Alessandro Resta <alessandro.resta@gmail.com>
2017-04-15 14:16:35 +0100

    Add main.go

--- main.go
+++ main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
+	scout "github.com/project-regista/scout/client"
 	"github.com/project-regista/scout/competition"
 )
 
 func main() {
-	competition.GetCompetitions()
+
+	// Get Scout client
+	client, err := scout.New("./client/config.yml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Competition by ID
+	comp, err := competition.GetCompetition(client, "43")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Competition by ID: %+v\n", comp)
+
+	// List of Competitions
+	comps, err := competition.GetCompetitions(client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("List of Competitions: %+v\n", comps)
+
+	// List Competitions w/ Seasons
+	compsSeasons, err := competition.GetCompetitionsSeasons(client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("List of Competitions w/ Seasons: %+v\n", compsSeasons)
 }
