f9ae9a6caffe2ab1e35e629a786ebbb1e6bf358e
Alessandro Resta <alessandro.resta@gmail.com>
2017-04-20 20:53:57 +0100

    Update package call and call country methods

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 
 	scout "github.com/project-regista/scout/client"
-	"github.com/project-regista/scout/competition"
+	"github.com/project-regista/scout/request"
 )
 
 func main() {
@@ -17,23 +17,41 @@ func main() {
 	}
 
 	// Competition by ID
-	comp, err := competition.GetCompetition(client, "43")
+	competition, err := request.GetCompetition(client, "43")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Competition by ID: %+v\n", comp)
+	fmt.Printf("Competition by ID: %+v\n", competition)
 
 	// List of Competitions
-	comps, err := competition.GetCompetitions(client)
+	competitions, err := request.GetCompetitions(client)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("List of Competitions: %+v\n", comps)
+	fmt.Printf("List of Competitions: %+v\n", competitions)
 
 	// List Competitions w/ Seasons
-	compsSeasons, err := competition.GetCompetitionsSeasons(client)
+	competitionsSeasons, err := request.GetCompetitionsSeasons(client)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("List of Competitions w/ Seasons: %+v\n", compsSeasons)
+	fmt.Printf("List of Competitions w/ Seasons: %+v\n", competitionsSeasons)
+
+	country, err := request.GetCountry(client, "13")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Country by ID: %+v\n", country)
+
+	countries, err := request.GetCountries(client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("List of Countries: %+v\n", countries)
+
+	countriesCompetitions, err := request.GetCountriesCompetitions(client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("List of Countries w/ Competitions: %+v\n", countriesCompetitions)
 }
